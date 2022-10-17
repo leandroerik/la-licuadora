8cbda2c5096f4375611ac0c4598185bb835a0378
LeandroErik <erikquispechoque@gmail.com>
2022-10-16 23:48:20 -0300

    persisto al area personalizacion

--- src/main/java/domain/modelos/personalizacion/AreaPersonalizacion.java
+++ src/main/java/domain/modelos/personalizacion/AreaPersonalizacion.java
@@ -1,17 +1,21 @@
 package domain.modelos.personalizacion;
 
-public class AreaPersonalizacion {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Persistence;
+
+@Entity(name = "area_personalizacion")
+@Getter
+@Setter
+public class AreaPersonalizacion extends Persistence {
+    @Column(name="desc_area")
     String descripcionArea;
 
     public AreaPersonalizacion(String descripcionArea) {
         this.descripcionArea = descripcionArea;
     }
 
-    public String getDescripcionArea() {
-        return descripcionArea;
-    }
-
-    public void setDescripcionArea(String descripcionArea) {
-        this.descripcionArea = descripcionArea;
-    }
 }
