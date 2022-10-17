6a4f934293b817761113edb4293bb629bb34d654
LeandroErik <erikquispechoque@gmail.com>
2022-10-17 00:17:28 -0300

    mapeo bidireccional OneToMany de gestor-productos

--- src/main/java/domain/modelos/Gestor.java
+++ src/main/java/domain/modelos/Gestor.java
@@ -3,10 +3,7 @@ package domain.modelos;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 @Entity
@@ -14,10 +11,12 @@ import java.util.List;
 @Getter
 @Setter
 public class Gestor extends Persistente {
+
     @Column(name="nombre")
-    String nombre;
-    @Transient
-    List<ProductoBase> productos;
+    private String nombre;
+
+    @OneToMany(mappedBy = "gestor")
+    private List<ProductoBase> productos;
 
     public Gestor(String nombre) {
         this.nombre = nombre;
--- src/main/java/domain/modelos/ProductoBase.java
+++ src/main/java/domain/modelos/ProductoBase.java
@@ -4,31 +4,33 @@ import domain.modelos.personalizacion.PosiblePersonalizacion;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-@Entity(name="producto_base")
+@Entity
+@Table(name="producto_base")
 @Getter
 @Setter
-public class ProductoBase {
+public class ProductoBase extends Persistente{
+
     @Column(name = "nombre")
-    String nombre;
+    private String nombre;
 
     @Column(name = "descripcion")
-    String descripcion;
+    private String descripcion;
 
     @Transient
     List<PosiblePersonalizacion> personalizacionesPermitidas;
 
     @Column(name="tiempo_fabricacion")
-    int tiempoFabricacion;
+    private int tiempoFabricacion;
 
     @Column(name = "precio_base")
-    int precioBase;
+    private int precioBase;
 
+    @ManyToOne
+    @JoinColumn(name="gestor_id",referencedColumnName = "id")
+    private Gestor gestor;
 
     public ProductoBase(String nombre, String descripcion, int tiempoFabricacion, int precioBase) {
         this.nombre = nombre;
